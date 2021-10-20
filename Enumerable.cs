ab98802806cddf5fa154af003be15d0e14cb36a8
Ihor Moroz <ihor.moroz.17@gmail.com>
2021-10-20 12:53:41 +0300

    Add project files.

---
+++ Enumerable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms
+{
+    public static class Enumerable
+    {
+        public static IEnumerable<TResult> Select<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        {
+            foreach (TSource item in source)
+                yield return selector(item);
+        }
+    }
+}
---
+++ Statistics.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms
+{
+    public static class Statistics
+    {
+        public static T Mode<T>(IEnumerable<T> selection)
+        {
+            var occurrences = new Dictionary<T, int>();
+            foreach (T item in selection)
+            {
+                if (occurrences.ContainsKey(item)) occurrences[item]++;
+                else occurrences[item] = 1;
+            }
+
+            var enumerator = selection.GetEnumerator();
+            enumerator.MoveNext();
+            T max = enumerator.Current;
+            while (enumerator.MoveNext())
+                if (occurrences[max] < occurrences[enumerator.Current])
+                    max = enumerator.Current;
+
+            return max;
+        }
+    }
+}
