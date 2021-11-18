132f777a6d9e1121041db967abe178d2946b4c1f
Ihor Moroz <ihor.moroz.17@gmail.com>
2021-11-18 08:17:48 +0200

    refactoring

--- Sorting.cs
+++ Sorting.cs
@@ -1,27 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Algorithms
 {
     public static class Sorting
     {
-        public delegate int DComparer<T>(T obj1, T obj2);
-
-        public static void BubbleSort(this object[] arr, DComparer<object> compare)
+        public static void Bubble<T>(this T[] arr, Comparison<T> compare)
         {
-            bool flag;
-
             for (int i = 0; i < arr.Length - 1; ++i)
             {
-                flag = false;
+                bool flag = false;
                 for (int j = 0; j < arr.Length - i - 1; ++j)
                 {
                     if (compare(arr[j], arr[j + 1]) > 0)
                     {
-                        (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+                        Swap(ref arr[j], ref arr[j + 1]);
                         flag = true;
                     }
                 }
@@ -29,23 +22,141 @@ namespace Algorithms
             }
         }
 
-        public static void BubbleSort<T>(this T[] arr, DComparer<T> compare)
+        public static void Selection<T>(this T[] arr, Comparison<T> compare)
         {
-            bool flag;
-
             for (int i = 0; i < arr.Length - 1; ++i)
             {
-                flag = false;
-                for (int j = 0; j < arr.Length - i - 1; ++j)
+                int extremeIndex = i + 1;
+                for (int j = i + 2; j < arr.Length; ++j)
                 {
-                    if (compare(arr[j], arr[j + 1]) > 0)
-                    {
-                        (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
-                        flag = true;
-                    }
+                    if (compare(arr[j], arr[extremeIndex]) < 0)
+                        extremeIndex = i;
                 }
-                if (flag == false) break;
+
+                if (compare(arr[extremeIndex], arr[i]) < 0)
+                    Swap(ref arr[extremeIndex], ref arr[i]);
             }
+        }
+
+        public static void Merge<T>(this T[] arr, Comparison<T> compare)
+        {
+            MergeRec(arr, 0, arr.Length, compare);
+        }
+
+        public static void Quick<T>(this T[] arr, Comparison<T> compare)
+        {
+            QuickRec(arr, 0, arr.Length - 1, compare);
+        }
+
+        public static void Shell<T>(this T[] arr, Comparison<T> compare)
+        {
+            for (int gap = arr.Length / 2; gap > 0; gap /= 2)
+            {
+                for (int i = gap; i < arr.Length; i += gap)
+                {
+                    T val = arr[i];
+                    int newPos = i;
+
+                    while (newPos > 0 && compare(val, arr[newPos - gap]) < 0)
+                    {
+                        arr[newPos] = arr[newPos - gap];
+                        newPos -= gap;
+                    }
+                    arr[newPos] = val;
+                }
+            }
+        }
+
+        public static void Counting<T>(this T[] arr, Comparison<T> compare)
+        {
+            Dictionary<T, int> dict = new();
+
+            foreach (T item in arr)
+            {
+                if (dict.ContainsKey(item)) dict[item]++;
+                else dict[item] = 1;
+            }
+
+            T[] unique = new T[dict.Keys.Count];
+            dict.Keys.CopyTo(unique, 0);
+
+            Quick(unique, compare);
+
+            int index = 0;
+            foreach (T item in unique)
+                for (int j = 0; j < dict[item]; ++j)
+                    arr[index++] = item;
+        }
+
+        static void Swap<T>(ref T a, ref T b)
+        {
+            T tmp = a;
+            a = b;
+            b = tmp;
+        }
+
+        static void QuickRec<T>(T[] arr, int first, int last, Comparison<T> compare)
+        {
+            if (last - first < 1) return;
+
+            T pivot = arr[last];
+            int partIndex = first;
+
+            for (int i = first; i < last; ++i)
+            {
+                if (compare(arr[i], pivot) < 0 && i != partIndex++)
+                    Swap(ref arr[i], ref arr[partIndex - 1]);
+            }
+
+            Swap(ref arr[last], ref arr[partIndex]);
+
+            QuickRec(arr, first, partIndex - 1, compare);
+            QuickRec(arr, partIndex + 1, last, compare);
+
+            //T pivot = arr[(first + last) / 2];
+            //int left = first, right = last;
+
+            //while (true)
+            //{
+            //    while (compare(arr[left], pivot) < 0)
+            //        left++;
+
+            //    while (compare(arr[right], pivot) > 0)
+            //        right--;
+
+            //    if (left == right) break;
+            //    if (compare(arr[left], arr[right]) == 0)
+            //        left++;
+            //    else
+            //        Swap(ref arr[left], ref arr[right]);
+            //}
+
+            //QuickRec(arr, first, right - 1, compare);
+            //QuickRec(arr, right + 1, last, compare);
+        }
+
+        static void MergeTwo<T>(T[] arr, int start, int m, int end, Comparison<T> compare)
+        {
+            T[] sorted = new T[end - start];
+
+            for (int i = 0, l = start, r = m; i < end - start;)
+            {
+                sorted[i++] = compare(arr[l], arr[r]) < 0 ? arr[l++] : arr[r++];
+                if (l == m) while (r != end) sorted[i++] = arr[r++];
+                if (r == end) while (l != m) sorted[i++] = arr[l++];
+            }
+
+            sorted.CopyTo(arr, start);
+        }
+
+        static void MergeRec<T>(T[] arr, int start, int end, Comparison<T> compare)
+        {
+            if (end - start < 2) return;
+
+            int m = (end - start) / 2 + start;
+            MergeRec(arr, start, m, compare);
+            MergeRec(arr, m, end, compare);
+            MergeTwo(arr, start, m, end, compare);
         }
     }
 }
